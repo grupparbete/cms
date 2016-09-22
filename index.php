84373051fc8582c695da71e964605be4ae1235b3
maxemiliang <maxemiliang@gmail.com>
2016-09-22 08:48:35 +0300

    add index instead of uploadform.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <meta charset="utf-8">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.3.1/jquery.min.js"></script>
   <script type="text/javascript" src="js/script.js"></script>
-  <link rel="stylesheet" href="../css/style.css" media="screen">
+  <link rel="stylesheet" href="public/css/style.css" media="screen">
 </head>
 <body>
 
