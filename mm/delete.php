e6da62b19be8ecaa98967e056d2c3697054554ac
maxemiliang <maxemiliang@gmail.com>
2016-09-23 09:47:26 +0300

    lul memem))

--- mm/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'mm.php';
-
-$db = new Db();
-$response =$db->delete_by_id($_GET['id']);
-header("location: index.php");
--- mm/update.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'mm.php';
-$db = new Db();
-$response = $db->update_by_id($_POST['id'], $_POST['description']);
-?>
