22fd1c6f731b751a35c3f81be295ab331642d000
maxemiliang <maxemiliang@gmail.com>
2016-09-23 11:21:56 +0300

    dank meme(( 8

--- views/menu.php
+++ views/menu.php
@@ -6,4 +6,13 @@ include "head.php";
 
 <div class="container">
   <h1>Menu</h1>
+  <br>
+  <img class="img-responsive img-rounded" src="img/menu1.jpg" alt="menu1">
+  <h2>Kaffe</h2>
 </div>
+
+  <?php 
+  
+    include "footer.php";
+
+  ?> 
