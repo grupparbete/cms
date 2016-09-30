0e71a81de19e9a2b45126cd856dec203c18a3ef0
maxemiliang <maxemiliang@gmail.com>
2016-09-30 10:17:54 +0300

    suka

--- dump.sql
+++ dump.sql
@@ -1,36 +1,109 @@
--- Adminer 4.2.4 MySQL dump
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Värd: 127.0.0.1
+-- Tid vid skapande: 30 sep 2016 kl 08:59
+-- Serverversion: 10.1.9-MariaDB
+-- PHP-version: 5.6.15
 
-SET NAMES utf8;
-SET time_zone = '+00:00';
-SET foreign_key_checks = 0;
-SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-DROP DATABASE IF EXISTS `coffeshop`;
-CREATE DATABASE `coffeshop` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databas: `coffeshop`
+--
+CREATE DATABASE IF NOT EXISTS `coffeshop` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `coffeshop`;
 
-DROP TABLE IF EXISTS `articles`;
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `articles`
+--
+
 CREATE TABLE `articles` (
-  `aID` int(11) NOT NULL AUTO_INCREMENT,
+  `aID` int(11) NOT NULL,
   `title` varchar(100) NOT NULL,
   `text` mediumtext NOT NULL,
   `img` varchar(40) NOT NULL DEFAULT 'default.jpg',
   `userID` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  PRIMARY KEY (`aID`),
-  KEY `userID` (`userID`)
+  `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-TRUNCATE `articles`;
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `users`;
+--
+-- Tabellstruktur `menu`
+--
+
+CREATE TABLE `menu` (
+  `menu` int(11) UNSIGNED NOT NULL,
+  `menu_header` varchar(30) NOT NULL,
+  `img` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `users`
+--
+
 CREATE TABLE `users` (
-  `uID` int(11) NOT NULL AUTO_INCREMENT,
+  `uID` int(11) NOT NULL,
   `username` varchar(60) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `privileges` smallint(6) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`uID`)
+  `privileges` smallint(6) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Index för dumpade tabeller
+--
 
--- 2016-03-01 10:34:41
+--
+-- Index för tabell `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`aID`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- Index för tabell `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`menu`);
+
+--
+-- Index för tabell `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`uID`);
+
+--
+-- AUTO_INCREMENT för dumpade tabeller
+--
+
+--
+-- AUTO_INCREMENT för tabell `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `aID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+--
+-- AUTO_INCREMENT för tabell `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `menu` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+--
+-- AUTO_INCREMENT för tabell `users`
+--
+ALTER TABLE `users`
+  MODIFY `uID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
